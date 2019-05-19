cfb753b6d14d31f0332809f9f30e29650074d5d8
YairFernando67 <yair.facio11@gmail.com>
2019-05-18 22:02:17 -0500

    List Module-RazorPages

--- BookListRazor/BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDBContext _db;
 
+        public IndexModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<BookViewModel> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
         }
     }
 }
